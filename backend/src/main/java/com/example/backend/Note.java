048811380000202c7794e85e8ef73cbbfe1713fb
hajunk05 <hajunk05@gmail.com>
2025-04-11 20:36:49 +1200

    GET /notes

---
+++ backend/src/main/java/com/example/backend/Note.java
@@ -0,0 +1,14 @@
+package com.example.backend;
+
+import java.util.List;
+
+public class Note {
+
+    public String title;
+    public String description;
+
+    public Note(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+}
---
+++ backend/src/main/java/com/example/backend/NoteController.java
@@ -0,0 +1,20 @@
+package com.example.backend;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class NoteController {
+
+    @GetMapping("/notes")
+    public List<Note> getNotes() {
+        List<Note> note = new ArrayList<>();
+        note = List.of(new Note("Title", "Description"), new Note("Titles", "Descriptions"));
+        return note;
+    }
+
+}
