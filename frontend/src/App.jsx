7d262ed0feed4a922048cec53f1c15efe02251a6
hajunk05 <hajunk05@gmail.com>
2025-04-11 21:00:22 +1200

    wired backend with frontend for render

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, {useEffect, useState} from "react";
+import axios from "axios";
+import Note from "./Note.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:8080/api/notes")
+        .then(res => {
+          setNotes(res.data);
+        })
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        {notes.map(note => <Note note={note}/>)}
     </>
   )
 }
